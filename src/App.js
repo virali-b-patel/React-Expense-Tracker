2f43e7ea37133adfbf43d245985a8e464f68c8dd
virali-b-patel <virali678@gmail.com>
2023-03-24 14:37:44 +0530

    React Expense Tracker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import HomeComponent from "./modules/home";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px 0 10px;
+  font-family: Montserrat;
+`;
+
+const Header = styled.span`
+  color: black;
+  font-size: 25px;
+  font-weight: bold;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>Expense Tracker</Header>
+      <HomeComponent />
+    </Container>
   );
 }
 
---
+++ src/modules/home/index.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import OverViewComponent from "./OverViewComponent";
+import TransactionComponent from "./TransactionComponent";
+import { useEffect, useState } from "react";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px 0 10px;
+  font-family: Montserrat;
+  width: 360px;
+`;
+
+const HomeComponent = (props) => {
+  const [transactions, updateTransaction] = useState([]);
+  const [expense, updateExpense] = useState(0);
+  const [income, updateIncome] = useState(0);
+
+  const addTransaction = (payload) => {
+    const transactionArray = [...transactions];
+    transactionArray.push(payload);
+    updateTransaction(transactionArray);
+  };
+
+  const calculateBalance = () => {
+    let exp = 0;
+    let inc = 0;
+    transactions.map((payload) => {
+      payload.type === "EXPENSE"
+        ? (exp = exp + payload.amount)
+        : (inc = inc + payload.amount);
+    });
+    updateExpense(exp);
+    updateIncome(inc);
+  };
+
+  useEffect(() => calculateBalance(), [transactions]);
+
+  return (
+    <Container>
+      <OverViewComponent
+        addTransaction={addTransaction}
+        expense={expense}
+        income={income}
+      />
+      <TransactionComponent transactions={transactions} />
+    </Container>
+  );
+};
+
+export default HomeComponent;
